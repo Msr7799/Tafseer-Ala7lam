e7e38016a679a1bf0cf97fc093d3cb5d78847599
unknown <alromaihi2224@gmail.com>
2026-05-12 23:21:41 +0300

    firebase config.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,14 +5,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB16TJ4aBNjdE_xPmdXlTRBpNQthoHFagA",
-  authDomain: "tafseer-ala7lam.firebaseapp.com",
-  databaseURL: "https://tafseer-ala7lam-default-rtdb.firebaseio.com",
-  projectId: "tafseer-ala7lam",
-  storageBucket: "tafseer-ala7lam.firebasestorage.app",
-  messagingSenderId: "111930949337",
-  appId: "1:111930949337:web:d043dbe4bb27b427f108c8",
-  measurementId: "G-KHPNSVMJHL"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyB16TJ4aBNjdE_xPmdXlTRBpNQthoHFagA",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "tafseer-ala7lam.firebaseapp.com",
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL || "https://tafseer-ala7lam-default-rtdb.firebaseio.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "tafseer-ala7lam",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "tafseer-ala7lam.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "111930949337",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:111930949337:web:d043dbe4bb27b427f108c8",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-KHPNSVMJHL"
 };
 
 export const firebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
